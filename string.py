c49c8fb7ca6fedbf33e4c74a5c98241a30c9c640
Parag-deb <paragdeb.pdeb@gmail.com>
2025-04-22 20:22:57 +0600

    done

--- string.py
+++ string.py
@@ -10,3 +10,16 @@ print(len(a)) #length
 print(a[len(a)-1])
 print(a[-1])
 
+#python is immutable, string declare hole r change kora jay na
+
+b = 'DINgo'
+print(b.swapcase())
+
+#sreing format
+age = 23
+birthday = '20 February'
+user_input = input("What is your name?")
+print("Hello, " + user_input + "!")
+
+txt = "I'm {user_input}. I'm {age} years old . My birthday is on {birthday}".format(user_input=user_input , age = age , birthday=birthday)
+print(txt)
