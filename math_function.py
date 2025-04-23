f4d4e69799b14cfd1e4d57514ff6c7c2a77bf75b
Parag-deb <paragdeb.pdeb@gmail.com>
2025-04-23 22:34:58 +0600

    done

---
+++ math_function.py
@@ -0,0 +1,14 @@
+import math
+
+x = 4.5
+
+print(math.ceil(x))
+
+print(math.floor(x))
+
+print(round(x))
+
+y = 4.6
+print(round(y))
+
+print(math.sqrt(x))
--- operator.py
+++ operator.py
@@ -6,3 +6,6 @@ e = 21//5 # integer division
 print(e) 
 print(float(e))
 
+# precedence - BODMAS (Bracket , Order/exponential , division, multiplication, add, sub)
+g = 5**4 #5 to the power 4
+print(g) 
