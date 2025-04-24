c26c2b2babba6326e54df995f6ee005927200e7a
Parag-deb <paragdeb.pdeb@gmail.com>
2025-04-24 16:57:09 +0600

    done

---
+++ list_tuples.py
@@ -0,0 +1,19 @@
+# list -> we can store different data types
+# list is mutable , so we can change any data
+a = [1,2,3, 'Naim', 'Fahim', 8.9, 3.4]
+
+print(a)
+
+a[0] = 100
+
+print(a)
+
+s = "he llo"
+print(list(s))
+
+a.append([1,2,3])
+print(a)
+print(a.index('Naim'))
+
+a.reverse()
+print(a)
