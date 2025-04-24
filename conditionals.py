5528c7503f84352fa5961ad7aee21c6bc768695e
Parag-deb <paragdeb.pdeb@gmail.com>
2025-04-24 16:26:01 +0600

    done

---
+++ conditionals.py
@@ -0,0 +1,11 @@
+# = -> assignment operator
+# == -> comparison 
+
+print(10 == 4)
+print(10 == 10)
+
+rain = 1
+if(rain == 1):
+    print("ajk scholl e jabo na")
+else:
+    print("ajk school e jabo")
