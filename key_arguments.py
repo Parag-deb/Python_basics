92bfc6a28e5c0b5fca4afccfe87f81b5a8915141
Parag-deb <paragdeb.pdeb@gmail.com>
2025-04-28 23:54:56 +0600

    done

--- key_arguments.py
+++ key_arguments.py
@@ -2,3 +2,7 @@ def my_func(f_name , l_name, age):
     print(f"My name is {f_name} {l_name}. I'm {age} years old")
 
 my_func("Parag", "deb" ,23)
+
+# applying key argumrents
+
+my_func(age =23, l_name="deb", f_name="Parag")
