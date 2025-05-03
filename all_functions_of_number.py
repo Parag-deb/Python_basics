eca6fd3f57b6f14fd7d219dd3c684704bd50678c
Parag-deb <paragdeb.pdeb@gmail.com>
2025-05-03 23:35:13 +0600

    done

--- all_functions_of_number.py
+++ all_functions_of_number.py
@@ -1,5 +1,5 @@
 # number_basics.py
-
+import math
 # Integers
 x = 10
 print(f"1. Integer: {x}")  # 10
@@ -37,3 +37,9 @@ print(f"4. round(3.456): {round(3.456)}")  # 3
 
 # round() with ndigits
 print(f"5. round(3.456, 2): {round(3.456, 2)}")  # 3.46
+
+print(f"1. math.ceil(3.1): {math.ceil(3.1)}")  # 4
+print(f"2. math.floor(3.9): {math.floor(3.9)}")  # 3
+print(f"3. math.sqrt(16): {math.sqrt(16)}")  # 4.0
+print(f"4. math.factorial(5): {math.factorial(5)}")  # 120
+print(f"5. math.fabs(-5): {math.fabs(-5)}")  # 5.0
