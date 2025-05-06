ef47ab4f7c6adb832bad32b1f502462aacac24ed
Parag-deb <paragdeb.pdeb@gmail.com>
2025-05-06 10:45:40 +0600

    done

--- Basics/OOP/solutions.py
+++ Basics/OOP/solutions.py
@@ -1,7 +1,10 @@
-# create a car c;ass with attributes like brand and model, 
-# then create an instance of this class
+# 1.create a car c;ass with attributes like brand and model, 
+#   then create an instance of this class
 
-# add method to the car class that displays the full name of the car( brand and model)
+# 2.add method to the car class that displays the full name of the car( brand and model)
+# 3. Create an ElectricCar class that inherits from the car class and has an additional attributes battery_size
+# 4. Encapsulation - Modify the car class to encapsulate the brand attribute, making it private and provide a getter method of it 
+
 # class Car:
 #     brand = None
 #     model = None
@@ -12,8 +15,8 @@ class Car:
         self.model = model
     
     def full_name(self):
-        return f"BRAND: {self.brand} MODEL:{self.model}"
-        
+        return f"BRAND: {self.brand} MODEL: {self.model}"
+    
 
 
 my_car = Car("Toyota", "Corolla")
@@ -24,6 +27,20 @@ print(my_car.full_name())
 my_new_car = Car("Tata", "Safari")
 print(my_new_car.brand , my_new_car.model)
 print(my_new_car.full_name())
+
+# Quertion 3
+print()
+print("-----inheritance - Question 3-----")
+
+class ElectricCar(Car):
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand, model)
+        self.battery_size = battery_size
+        
+my_tesla = ElectricCar("Tesla", "Model S", "85kwh")
+print(my_tesla.full_name())
+
+
 
 
 
